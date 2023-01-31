e3034826fac4beb66363be3eb8bb21a59f9d2386
Vu <doragan8@gmail.com>
2023-01-31 15:46:51 +0700

    finish video 9&10

---
+++ Assessment1/Assets/Script/Credit.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credit : MonoBehaviour
+{
+    public void Quit()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+}
